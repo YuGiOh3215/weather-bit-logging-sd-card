79fbcd35ccd1f07c2632ebf4fdb4244d301feb62
ERoy <131496746+YuGiOh3215@users.noreply.github.com>
2023-05-13 22:19:39 -0400

    Added more class to streamline code

--- main.py
+++ main.py
@@ -10,6 +10,46 @@ from tkinter import *
 from time import *
 from logging import *
 
+class LoggingParams:
+    listWindSpeed = [0, 5, 10, 20, 40, 50]
+    idefaultLogInterv = 5000
+    iHighLogInterv = 1000
+    iLogInterval = 0
+
+    def __init__(self, iLevel, iCount):
+
+        self.iLevel = iLevel
+        self.iCount = iCount
+        self.iLogInterval = self.idefaultLogInterv
+
+    def getLogInterval(self):
+        return (self.iLogInterval)
+
+    def setLogIntervalToHigh(self):
+        self.iLogInterval = self.iHighLogInterv
+ 
+    def setLogIntervalToStd(self):
+         self.iLogInterval = self.idefaultLogInterv
+
+    def getWindSpeedThreshold(self):
+        return (self.listWindSpeed[self.iLevel])
+
+    def increaseLevel(self):
+        
+        if self.iLevel == (len(self.listWindSpeed)-1):
+            self.iLevel = 0
+        else:
+            self.iLevel = self.iLevel + 1
+
+    def continueLogging(self):
+        if (self.iCount < 20):
+            self.iCount = self.iCount + 1
+            return True
+        else:
+            return False
+
+        
+
 def showLoggingLED():
     basic.show_leds("""
         . . . . .
@@ -29,12 +69,35 @@ def showNotLoggingLED():
         # . . . #
     """)
 
-def showWindLevel():
-    global iLevel
-        
-    if (iLevel == 0):
-        serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE9600)
+def showQMarkLED():
+    basic.show_leds("""
+        . # # # .
+        . # # #.
+        . . # . .
+        . . . . .
+        . . # . .
+    """)
 
+def show3DotsLED():
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        # . # . #
+        . . . . .
+        . . . . .
+    """)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        . . . . .
+    """)
+
+def showWindLevel():
+    global p1
+        
+    if (p1.iLevel == 0):
         basic.show_leds("""
             . . . . .
             . . . . .
@@ -42,9 +105,7 @@ def showWindLevel():
             . . . . .
             # # # # #
             """)
-    elif (iLevel == 1):
-#        serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE28800)
-
+    elif (p1.iLevel == 1):
         basic.show_leds("""
             . . . . .
             . . . . .
@@ -52,8 +113,7 @@ def showWindLevel():
             . . . . .
             . . # . .
             """)
-    elif (iLevel == 2):
-#        serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE38400)
+    elif (p1.iLevel == 2):
         basic.show_leds("""
             . . . . .
             . . . . .
@@ -61,8 +121,7 @@ def showWindLevel():
             . . # . .
             . . # . .
             """)
-    elif (iLevel == 3):
- #       serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE57600)
+    elif (p1.iLevel == 3):
         basic.show_leds("""
             . . . . .
             . . . . .
@@ -70,8 +129,7 @@ def showWindLevel():
             . . # . .
             . . # . .
             """)
-    elif (iLevel == 4):
-  #      serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE14400)
+    elif (p1.iLevel == 4):
         basic.show_leds("""
             . . . . .
             . . # . .
@@ -79,8 +137,7 @@ def showWindLevel():
             . . # . .
             . . # . .
             """)
-    elif (iLevel == 5):
-#        serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE115200)
+    elif (p1.iLevel == 5):
         basic.show_leds("""
             . . # . .
             . . # . .
@@ -89,48 +146,72 @@ def showWindLevel():
             . . # . .
             """)
     
+class dataOutput:
+    def __init__(self):
+        self.szLine = ""
+
+    def writeHeader(self):
+        szLine = 'WSP,CWD,TiC,HUM,PRESS'
+        serial.write_line(szLine)
+
+    def writeData(self,WSP, CWD, TiC, HUM, PRESS):
+        szLine = WSP + ',' + \
+            CWD + ',' + \
+            TiC + ',' + \
+            HUM + ',' + \
+            PRESS
+
+        serial.write_line(szLine)
 
 
 def on_button_pressed_a():
-    global TurnLoggingOnOff
-    TurnLoggingOnOff = not (TurnLoggingOnOff)
-    if (TurnLoggingOnOff == True):
+    global LoggingIsOn
+    LoggingIsOn = not (LoggingIsOn)
+    
+    if (LoggingIsOn == True):
         showWindLevel()
-
+        basic.pause(2000)
+        showLoggingLED()
+    else:
+        showNotLoggingLED()
+        
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    global minWindSpeed, listWindSpeed, iLevel
+    global p1
     
-    if iLevel == len(listWindSpeed)-1:
-        iLevel = 0
-    else:
-        iLevel = iLevel + 1
+    if LoggingIsOn == False:
+        p1.increaseLevel()
+        
+        minWindSpeed = p1.getWindSpeedThreshold()
 
-    minWindSpeed = listWindSpeed[iLevel]
+        szLine = '--------- limit ' + minWindSpeed + '---------'
+        serial.write_line(szLine)    
+
+        showWindLevel()
+        basic.pause(2000)
+        showNotLoggingLED()
+    else:
+        showQMarkLED()
+        basic.pause(2000)
+        showLoggingLED()
+        
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-                                           
-listWindSpeed = [5, 10, 20, 30, 40, 50]
-iLevel = 0
-current_WindDirection_List = ""
-current_WindSpeed = 0
-tempC = 0
-TurnLoggingOnOff = True
-szLine = ""
+
+
+p1 = LoggingParams(0,0)    
+dataLog = dataOutput()  
+
+LoggingIsOn = False
 doLog = False
-iCount = 0
-idefaultLogInterv = 5000
-iHighLogInterv = 1000
-minWindSpeed = listWindSpeed[iLevel]
 
-iLogInterval = idefaultLogInterv
-#serial.redirect_to_usb()
-
-serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE9600)
 weatherbit.start_wind_monitoring()
 weatherbit.start_weather_monitoring()
-TurnLoggingOnOff = False
+
+serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE9600)
+#serial.redirect_to_usb()
+
 """
 
 Note: If "???" is displayed, direction is unknown!
@@ -139,63 +220,48 @@ Note: If "???" is displayed, direction is unknown!
 
 
 def on_forever():
-    global current_WindSpeed, current_WindDirection_List, minWindSpeed
-    global tempC, szLine, iLogInterval, iHighLogInterv, idefaultLogInterv
+    global p1, dataLog
 
-    # -------- wind --------
-    current_WindSpeed = weatherbit.wind_speed() * 3600 / 1000
+    tempC = 0
+    current_WindSpeed = 0.0
+    current_WindDirection_List = ""
 
-    if (current_WindSpeed > minWindSpeed):
-        doLog = True
-        iLogInterval = iHighLogInterv
-    elif (iCount < 20):
-        iCount = iCount + 1
-    else:
-        doLog = False
-        iLogInterval = idefaultLogInterv
-    
-    if TurnLoggingOnOff == True:
-        doLog = False
-        iLogInterval = idefaultLogInterv
-     
-    if doLog:
-    
-        showLoggingLED()
-        current_WindDirection_List = weatherbit.wind_direction()
+    if LoggingIsOn == True:
+        # -------- wind --------
+#        temperatureC = (weatherbit.temperature()/ 100)
+#        if (temperatureC > p1.getWindSpeedThreshold()):
+        current_WindSpeed = weatherbit.wind_speed() * 3600 / 1000
+        if (current_WindSpeed > p1.getWindSpeedThreshold()):
+            doLog = True
+            p1.setLogIntervalToHigh()
+        elif (p1.continueLogging() == False):
+            doLog = False
+            p1.setLogIntervalToStd()
 
-        # -------- temperature --------
-        tempC = (weatherbit.temperature()/ 100)
-        # -------- humidity --------
+        if doLog:
 
-        humid = (weatherbit.humidity()/ 1024)
-        # -------- pressure --------
-        pressure = (weatherbit.pressure()/ 25600)
-            
-        szLine = current_WindSpeed + ',' + \
-            current_WindSpeed + ',' + \
-            current_WindDirection_List + ',' + \
-            tempC + ',' + \
-            humid + ',' + \
-            pressure
+            showLoggingLED()
+            current_WindDirection_List = weatherbit.wind_direction()
 
-        serial.write_line(szLine)
-        
+            # -------- temperature --------
+            tempC = (weatherbit.temperature()/ 100)
+            # -------- humidity --------
+
+            humid = (weatherbit.humidity()/ 1024)
+            # -------- pressure --------
+            pressure = (weatherbit.pressure()/ 25600)
+
+            dataLog.writeData(current_WindSpeed, current_WindDirection_List,
+                tempC, humid, pressure)    
+
+        else:
+            show3DotsLED()
     else:
         showNotLoggingLED()
         
-        
-    basic.pause(iLogInterval)
+    basic.pause(p1.getLogInterval())
 
-serial.redirect_to_usb()
-#serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE9600)
-   
+dataLog.writeHeader()
 while True:
-#    on_forever()
-    temperatureC = 0
-    temperatureC = (weatherbit.temperature()/ 100)
-    pressure = (weatherbit.pressure()/ 25600)
-    szLine = temperatureC + ','+ pressure
-    serial.write_line(szLine)
-    #control.wait_micros(1000)
-    basic.pause(1000)
-    #display.scroll(x_strength)
+    on_forever()
+
--- main.ts
+++ main.ts
@@ -1,8 +1,105 @@
-let temperatureC: number;
-let pressure: number;
 // from math import *
 // from serial import *
 // from threaded import *
+class LoggingParams {
+    static iLogInterval: number
+    private ___iLogInterval_is_set: boolean
+    private ___iLogInterval: number
+    get iLogInterval(): number {
+        return this.___iLogInterval_is_set ? this.___iLogInterval : LoggingParams.iLogInterval
+    }
+    set iLogInterval(value: number) {
+        this.___iLogInterval_is_set = true
+        this.___iLogInterval = value
+    }
+    
+    static idefaultLogInterv: number
+    private ___idefaultLogInterv_is_set: boolean
+    private ___idefaultLogInterv: number
+    get idefaultLogInterv(): number {
+        return this.___idefaultLogInterv_is_set ? this.___idefaultLogInterv : LoggingParams.idefaultLogInterv
+    }
+    set idefaultLogInterv(value: number) {
+        this.___idefaultLogInterv_is_set = true
+        this.___idefaultLogInterv = value
+    }
+    
+    static iHighLogInterv: number
+    private ___iHighLogInterv_is_set: boolean
+    private ___iHighLogInterv: number
+    get iHighLogInterv(): number {
+        return this.___iHighLogInterv_is_set ? this.___iHighLogInterv : LoggingParams.iHighLogInterv
+    }
+    set iHighLogInterv(value: number) {
+        this.___iHighLogInterv_is_set = true
+        this.___iHighLogInterv = value
+    }
+    
+    static listWindSpeed: number[]
+    private ___listWindSpeed_is_set: boolean
+    private ___listWindSpeed: number[]
+    get listWindSpeed(): number[] {
+        return this.___listWindSpeed_is_set ? this.___listWindSpeed : LoggingParams.listWindSpeed
+    }
+    set listWindSpeed(value: number[]) {
+        this.___listWindSpeed_is_set = true
+        this.___listWindSpeed = value
+    }
+    
+    iLevel: number
+    iCount: number
+    public static __initLoggingParams() {
+        LoggingParams.listWindSpeed = [0, 5, 10, 20, 40, 50]
+        LoggingParams.idefaultLogInterv = 5000
+        LoggingParams.iHighLogInterv = 1000
+        LoggingParams.iLogInterval = 0
+    }
+    
+    constructor(iLevel: number, iCount: number) {
+        this.iLevel = iLevel
+        this.iCount = iCount
+        this.iLogInterval = this.idefaultLogInterv
+    }
+    
+    public getLogInterval(): number {
+        return this.iLogInterval
+    }
+    
+    public setLogIntervalToHigh() {
+        this.iLogInterval = this.iHighLogInterv
+    }
+    
+    public setLogIntervalToStd() {
+        this.iLogInterval = this.idefaultLogInterv
+    }
+    
+    public getWindSpeedThreshold(): number {
+        return this.listWindSpeed[this.iLevel]
+    }
+    
+    public increaseLevel() {
+        if (this.iLevel == this.listWindSpeed.length - 1) {
+            this.iLevel = 0
+        } else {
+            this.iLevel = this.iLevel + 1
+        }
+        
+    }
+    
+    public continueLogging(): boolean {
+        if (this.iCount < 20) {
+            this.iCount = this.iCount + 1
+            return true
+        } else {
+            return false
+        }
+        
+    }
+    
+}
+
+LoggingParams.__initLoggingParams()
+
 function showLoggingLED() {
     basic.showLeds(`
         . . . . .
@@ -23,10 +120,36 @@ function showNotLoggingLED() {
     `)
 }
 
+function showQMarkLED() {
+    basic.showLeds(`
+        . # # # .
+        . # # #.
+        . . # . .
+        . . . . .
+        . . # . .
+    `)
+}
+
+function show3DotsLED() {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        . . . . .
+        . . . . .
+    `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        . . . . .
+    `)
+}
+
 function showWindLevel() {
     
-    if (iLevel == 0) {
-        serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
+    if (p1.iLevel == 0) {
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -34,8 +157,7 @@ function showWindLevel() {
             . . . . .
             # # # # #
             `)
-    } else if (iLevel == 1) {
-        //         serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE28800)
+    } else if (p1.iLevel == 1) {
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -43,8 +165,7 @@ function showWindLevel() {
             . . . . .
             . . # . .
             `)
-    } else if (iLevel == 2) {
-        //         serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE38400)
+    } else if (p1.iLevel == 2) {
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -52,8 +173,7 @@ function showWindLevel() {
             . . # . .
             . . # . .
             `)
-    } else if (iLevel == 3) {
-        //        serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE57600)
+    } else if (p1.iLevel == 3) {
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -61,8 +181,7 @@ function showWindLevel() {
             . . # . .
             . . # . .
             `)
-    } else if (iLevel == 4) {
-        //       serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE14400)
+    } else if (p1.iLevel == 4) {
         basic.showLeds(`
             . . . . .
             . . # . .
@@ -70,8 +189,7 @@ function showWindLevel() {
             . . # . .
             . . # . .
             `)
-    } else if (iLevel == 5) {
-        //         serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE115200)
+    } else if (p1.iLevel == 5) {
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -79,98 +197,111 @@ function showWindLevel() {
             . . # . .
             . . # . .
             `)
+    }
+    
+}
+
+class dataOutput {
+    szLine: string
+    constructor() {
+        this.szLine = ""
+    }
+    
+    public writeHeader() {
+        let szLine = "WSP,CWD,TiC,HUM,PRESS"
+        serial.writeLine(szLine)
+    }
+    
+    public writeData(WSP: number, CWD: string, TiC: number, HUM: number, PRESS: number) {
+        let szLine = WSP + "," + CWD + "," + TiC + "," + HUM + "," + PRESS
+        serial.writeLine(szLine)
     }
     
 }
 
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    TurnLoggingOnOff = !TurnLoggingOnOff
-    if (TurnLoggingOnOff == true) {
+    LoggingIsOn = !LoggingIsOn
+    if (LoggingIsOn == true) {
         showWindLevel()
-    }
-    
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    if (iLevel == listWindSpeed.length - 1) {
-        iLevel = 0
-    } else {
-        iLevel = iLevel + 1
-    }
-    
-    minWindSpeed = listWindSpeed[iLevel]
-})
-let listWindSpeed = [5, 10, 20, 30, 40, 50]
-let iLevel = 0
-let current_WindDirection_List = ""
-let current_WindSpeed = 0
-let tempC = 0
-let TurnLoggingOnOff = true
-let szLine = ""
-let doLog = false
-let iCount = 0
-let idefaultLogInterv = 5000
-let iHighLogInterv = 1000
-let minWindSpeed = listWindSpeed[iLevel]
-let iLogInterval = idefaultLogInterv
-// serial.redirect_to_usb()
-serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
-weatherbit.startWindMonitoring()
-weatherbit.startWeatherMonitoring()
-TurnLoggingOnOff = false
-/** Note: If "???" is displayed, direction is unknown! */
-function on_forever() {
-    let doLog: boolean;
-    let iCount: number;
-    let humid: number;
-    let pressure: number;
-    
-    
-    //  -------- wind --------
-    current_WindSpeed = weatherbit.windSpeed() * 3600 / 1000
-    if (current_WindSpeed > minWindSpeed) {
-        doLog = true
-        iLogInterval = iHighLogInterv
-    } else if (iCount < 20) {
-        iCount = iCount + 1
-    } else {
-        doLog = false
-        iLogInterval = idefaultLogInterv
-    }
-    
-    if (TurnLoggingOnOff == true) {
-        doLog = false
-        iLogInterval = idefaultLogInterv
-    }
-    
-    if (doLog) {
+        basic.pause(2000)
         showLoggingLED()
-        current_WindDirection_List = weatherbit.windDirection()
-        //  -------- temperature --------
-        tempC = weatherbit.temperature() / 100
-        //  -------- humidity --------
-        humid = weatherbit.humidity() / 1024
-        //  -------- pressure --------
-        pressure = weatherbit.pressure() / 25600
-        szLine = current_WindSpeed + "," + current_WindSpeed + "," + current_WindDirection_List + "," + tempC + "," + humid + "," + pressure
-        serial.writeLine(szLine)
     } else {
         showNotLoggingLED()
     }
     
-    basic.pause(iLogInterval)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let minWindSpeed: number;
+    let szLine: string;
+    
+    if (LoggingIsOn == false) {
+        p1.increaseLevel()
+        minWindSpeed = p1.getWindSpeedThreshold()
+        szLine = "--------- limit " + minWindSpeed + "---------"
+        serial.writeLine(szLine)
+        showWindLevel()
+        basic.pause(2000)
+        showNotLoggingLED()
+    } else {
+        showQMarkLED()
+        basic.pause(2000)
+        showLoggingLED()
+    }
+    
+})
+let p1 = new LoggingParams(0, 0)
+let dataLog = new dataOutput()
+let LoggingIsOn = false
+let doLog = false
+weatherbit.startWindMonitoring()
+weatherbit.startWeatherMonitoring()
+serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600)
+// serial.redirect_to_usb()
+/** Note: If "???" is displayed, direction is unknown! */
+function on_forever() {
+    let doLog: boolean;
+    let humid: number;
+    let pressure: number;
+    
+    let tempC = 0
+    let current_WindSpeed = 0.0
+    let current_WindDirection_List = ""
+    if (LoggingIsOn == true) {
+        //  -------- wind --------
+        //         temperatureC = (weatherbit.temperature()/ 100)
+        //         if (temperatureC > p1.getWindSpeedThreshold()):
+        current_WindSpeed = weatherbit.windSpeed() * 3600 / 1000
+        if (current_WindSpeed > p1.getWindSpeedThreshold()) {
+            doLog = true
+            p1.setLogIntervalToHigh()
+        } else if (p1.continueLogging() == false) {
+            doLog = false
+            p1.setLogIntervalToStd()
+        }
+        
+        if (doLog) {
+            showLoggingLED()
+            current_WindDirection_List = weatherbit.windDirection()
+            //  -------- temperature --------
+            tempC = weatherbit.temperature() / 100
+            //  -------- humidity --------
+            humid = weatherbit.humidity() / 1024
+            //  -------- pressure --------
+            pressure = weatherbit.pressure() / 25600
+            dataLog.writeData(current_WindSpeed, current_WindDirection_List, tempC, humid, pressure)
+        } else {
+            show3DotsLED()
+        }
+        
+    } else {
+        showNotLoggingLED()
+    }
+    
+    basic.pause(p1.getLogInterval())
 }
 
-serial.redirectToUSB()
-// serial.redirect(SerialPin.P15, SerialPin.P14, BaudRate.BAUD_RATE9600)
+dataLog.writeHeader()
 while (true) {
-    //     on_forever()
-    temperatureC = 0
-    temperatureC = weatherbit.temperature() / 100
-    pressure = weatherbit.pressure() / 25600
-    szLine = temperatureC + "," + pressure
-    serial.writeLine(szLine)
-    // control.wait_micros(1000)
-    basic.pause(1000)
+    on_forever()
 }
